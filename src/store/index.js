677d23cf2ee3cd22d3e25837193b3cc47caf680f
Nikola <nikola_simic@msn.com>
2020-11-30 16:31:41 +0100

    chore: added results and stats

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,65 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const urlResults = 'https://roulette-service-dev.nsoft.com/config/b6410774-a252-4bbe-a781-43c2b54f31af?locale=en&app=RouletteWeb'
+const urlStats = 'https://roulette-service-dev.nsoft.com/stats/b6410774-a252-4bbe-a781-43c2b54f31af/daily/'
+
+const headers = {
+  method: 'GET',
+  redirect: 'follow'
+}
+
 export default new Vuex.Store({
   state: {
+    resultsHeaders: [
+      {
+        text: 'Display ID',
+        align: 'start',
+        value: 'displayId'
+      },
+      { text: 'Result', value: 'result' },
+      { text: 'Status', value: 'status.name' }
+    ],
+    rounds: [],
+    statsHeaders: [
+      {
+        text: 'Bet',
+        align: 'start',
+        value: 'bet'
+      },
+      { text: 'Count', value: 'count' }
+    ],
+    bets: []
   },
   mutations: {
+    setRounds (state, payload) {
+      if (!state.rounds.some((r) => r.displayId === payload.displayId)) {
+        state.rounds = [...state.rounds, payload]
+      }
+    },
+    setBets (state, payload) {
+      state.bets = Object.entries(payload).map((i) => { return { bet: i[0], ...i[1] } })
+      console.log(
+        state.bets
+      )
+    }
   },
   actions: {
+    async setRounds (state) {
+      const rounds = await fetch(urlResults, headers)
+      state.commit('setRounds', (await rounds.json()).round)
+    },
+    async setBets (state) {
+      const bets = await fetch(`${urlStats}${new Date().toISOString().substring(0, 10)}`, headers)
+      state.commit('setBets', (await bets.json()).bets)
+    }
   },
   modules: {
+  },
+  getters: {
+    getRounds: state => state.rounds,
+    getResultsHeaders: state => state.resultsHeaders,
+    getBets: state => state.bets,
+    getStatsHeaders: state => state.statsHeaders
   }
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,44 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Results</h1>
+    <v-data-table
+      :headers="headers"
+      :items="rounds"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
+    <h1>Statistics</h1>
+    <v-data-table
+      :headers="headersBets"
+      :items="bets"
+      :items-per-page="5"
+      class="elevation-1"
+    ></v-data-table>
   </div>
 </template>
+<script>
+export default {
+  // data () {
+  //   return {
+
+  //   }
+  // },
+  computed: {
+    rounds () {
+      return this.$store.getters.getRounds
+    },
+    headers () {
+      return this.$store.getters.getResultsHeaders
+    },
+    bets () {
+      return this.$store.getters.getBets
+    },
+    headersBets () {
+      return this.$store.getters.getStatsHeaders
+    }
+  }
+}
+</script>
+
+<style>
+</style>
